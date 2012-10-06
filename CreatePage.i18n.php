85f9571ed20d809daba13913bf7e2d05fc431c4c
Mathias Ertl <mati@fsinf.at>
2012-10-06 13:24:53 +0200

    Use TISS instead of TUWIS

--- CreatePage.i18n.php
+++ CreatePage.i18n.php
@@ -1,15 +1,15 @@
 <?php
 $messages = array(
         'en' => array(
-		'createpage' => 'Create page', 
+		'createpage' => 'Create page',
 		'create_page' => 'Create page', /* this is a) link text in Special:Specialpages and b) title on the view of the special page */
 		'emCreatePagePageTitle' => 'Create a new page',
 		'introduction' => 'This page allows you to create a new description of a course at the university, you simply have to fill out these few fields and hit "Create":
 * \'\'\'university:\'\'\' The university where the course is/was held.
-* \'\'\'title:\'\'\' The full title of the course (you may want to search [http://tuwis.tuwien.ac.at/zope/_ZopeId/23004543A3JC5VHWBwA/tpp/lv/lv/search/index_html tuwis] for it, if it was held at the TU of Vienna)
+* \'\'\'title:\'\'\' The full title of the course (you may want to search [https://tiss.tuwien.ac.at/course/courseList.xhtml TISS] for it, if it was held at the TU of Vienna)
 * \'\'\'type:\'\'\' The type of the course
 * \'\'\'professor(s):\'\'\' name one or more professors that held this particular course.
-** Always name the "official" head of the course as shown in tuwis
+** Always name the "official" head of the course as shown in TISS
 ** You may also name additional people that are visible in that course as well
 * \'\'\'optional info:\'\'\' In this optional field you can give additional information if a course is held several times and the courses are commonly distinguished with other phrases.
 
@@ -33,14 +33,14 @@ $messages = array(
 		'emCreatePagePageTitle' => 'LVA-Beschreibung hinzufügen',
 		'introduction' => 'Mit diesem Formular kannst du schnell und einfach eine neue LVA-Beschreibung erstellen, einfach die Felder ausfüllen und "Erstellen" klicken:
 * \'\'\'Universität:\'\'\' Die Universität auf der der Kurs gehalten wird.
-* \'\'\'LVA-Name:\'\'\' Der vollständige LVA-Name (kannst du für die TU Wien im [http://tuwis.tuwien.ac.at/zope/_ZopeId/23004543A3JC5VHWBwA/tpp/lv/lv/search/index_html tuwis] nachschlagen)
+* \'\'\'LVA-Name:\'\'\' Der vollständige LVA-Name (kannst du für die TU Wien im [https://tiss.tuwien.ac.at/course/courseList.xhtml TISS] nachschlagen)
 * \'\'\'Typ:\'\'\' Der offizielle Typ
 * \'\'\'Lehrende:\'\'\' Lehrende, die diese Lehrveranstaltung halten
-** Nenne immer zumindest den offiziellen LVA-Leiter der im tuwis aufscheint
+** Nenne immer zumindest den offiziellen LVA-Leiter der im TISS aufscheint
 ** Sind weitere Lehrende in der LVA stark involviert, kannst du sie ebenfalls nennen
 * \'\'\'optionale Zusatzinfo:\'\'\' Manche LVAs werden meistens nicht über den/die Lehrenden sondern ueber einen anderen Begriff identifiziert, hier kannst du \'\'\'optional\'\'\' diese Begriffe nennen.
 
-		
+
 		',
 		'missing_input' => '<b>Bitte alle Felder ausfüllen.</b>',
 		'namespace_header' => 'Universität:',
