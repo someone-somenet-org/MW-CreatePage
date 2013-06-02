e6acffc72c138500fced750043e1dd593e5266eb
Mathias Ertl <mati@fsinf.at>
2013-06-02 12:44:19 +0200

    add alias page

---
+++ CreatePage.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for myextension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Mathias Ertl
+ */
+$specialPageAliases[ 'en' ] = array(
+        'CreatePage' => array('CreatePage', 'Create page' ),
+);
+
+/** Deutsch
+ * @author Mathias Ertl
+ */
+$specialPageAliases[ 'de' ] = array(
+        'CreatePage' => array('LVA-Beschreibung_hinzufügen', 'LVA-Beschreibung hinzufügen' ),
+);
