3935a0a53fa570c59e52c42a49a939c6344afe3d
Mathias Ertl <mati@fsinf.at>
2014-11-27 17:11:26 +0100

    remove deprecated functions

--- SpecialCreatePage.php
+++ SpecialCreatePage.php
@@ -62,14 +62,14 @@ class SpecialCreatePage extends SpecialPage
 				else
 					$wgOut->redirect($redir->getFullURL() . '?action=edit' );
 			} else {
-				$this->addText( wfMsg('missing_input') );
+				$this->addText( wfMessage('missing_input')->text() );
 			}
 		}
 
-		$this->addText( wfMsg('introduction') );
+		$this->addText( wfMessage('introduction')->text() );
 
 // caused problems 2007-12-09
-		$wgOut->setPagetitle( wfMsg('emCreatePagePageTitle') );
+		$wgOut->setPagetitle( wfMessage('emCreatePagePageTitle')->text() );
 
 		global $wgScript;
 		$handler = $wgScript . '/' . MWNamespace::getCanonicalName(NS_SPECIAL) . ":" . SpecialPage::getLocalName( 'Create page' );
@@ -78,24 +78,24 @@ class SpecialCreatePage extends SpecialPage
 				<input type="hidden" name="was_submitted" value="true">
 				<table>
 					<tr>
-						<th>' . wfMsg('namespace_header') . '</th>
-						<th>' . wfMsg('newpage_title') . '</th>
-						<th>' . wfMsg('newpage_type') . '</th>
-						<th>' . wfMsg('newpage_suffix1') . '</th>
-						<th>' . wfMsg('newpage_suffix2') . '</th>
+						<th>' . wfMessage('namespace_header')->text() . '</th>
+						<th>' . wfMessage('newpage_title')->text() . '</th>
+						<th>' . wfMessage('newpage_type')->text() . '</th>
+						<th>' . wfMessage('newpage_suffix1')->text() . '</th>
+						<th>' . wfMessage('newpage_suffix2')->text() . '</th>
 					<tr>
 						<td><select name=namespace>' . $namespaces . '</select></td>
 						<td><input size=40 type=\'text\' name=\'newtitle\' value=\'' . $newtitle . '\'></td>
 						<td><select name=type>' . $types . '</select></td>
 						<td><input size=15 type=\'text\' name=\'suffix1\' value=\'' . $suffix1 . '\'></td>
 						<td><input size=22 type=\'text\' name=\'suffix2\' value=\'' . $suffix2 . '\'></td>
-						<td><input type=\'submit\' value=\'' . wfMsg('button')  . '\'></td>
+						<td><input type=\'submit\' value=\'' . wfMessage('button')->text()  . '\'></td>
 					</tr>
 					<tr>
 						<td></td>
-						<td>' . wfMsg( 'newtitle_desc' ) . '</td>
+						<td>' . wfMessage( 'newtitle_desc' )->text() . '</td>
 						<td></td>
-						<td>' . wfMsg( 'suffix1_desc' ) . '</td>
+						<td>' . wfMessage( 'suffix1_desc' )->text() . '</td>
 						<td></td>
 					</tr>
 				</table></form>');
